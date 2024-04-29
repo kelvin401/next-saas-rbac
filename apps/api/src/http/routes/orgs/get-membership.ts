38be67e7c36e0ef7a8617153d73c6ba856c120d3
Kelvin Carvalho <kelvin.nirvana@gmail.com>
2024-04-29 20:41:12 -0300

    feat: route to get membership

---
+++ apps/api/src/http/routes/orgs/get-membership.ts
@@ -0,0 +1,46 @@
+import type { FastifyInstance } from 'fastify'
+import type { ZodTypeProvider } from 'fastify-type-provider-zod'
+
+import { auth } from '@/http/middlewares/auth'
+import { z } from 'zod'
+import { roleSchema } from '@saas/auth'
+
+export async function getMembership(app: FastifyInstance) {
+  app
+    .withTypeProvider<ZodTypeProvider>()
+    .register(auth)
+    .get(
+      '/organizations/:slug/membership',
+      {
+        schema: {
+          tags: ['Organization'],
+          summary: 'Get user membership on organization.',
+          security: [{ bearerAuth: [] }],
+          params: z.object({
+            slug: z.string(),
+          }),
+          response: {
+            200: z.object({
+              membership: z.object({
+                id: z.string().uuid(),
+                role: roleSchema,
+                organizationId: z.string(),
+              }),
+            }),
+          },
+        },
+      },
+      async (request) => {
+        const { slug } = request.params
+        const { membership } = await request.getUserMembership(slug)
+
+        return {
+          membership: {
+            id: membership.id,
+            role: roleSchema.parse(membership.role),
+            organizationId: membership.organizationId,
+          },
+        }
+      }
+    )
+}
