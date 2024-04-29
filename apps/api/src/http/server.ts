d3b8794f37f0d6302dee11407ae62cf82b21a7b7
Kelvin Carvalho <kelvin.nirvana@gmail.com>
2024-04-29 19:53:56 -0300

    feat: route to create organizations

--- apps/api/src/http/server.ts
+++ apps/api/src/http/server.ts
@@ -18,6 +18,7 @@ import { requestPasswordRecover } from './routes/auth/request-password-recover'
 import { resetPassword } from './routes/auth/reset-password'
 import { authenticateWithGithub } from './routes/auth/authenticate-with-github'
 import { env } from '@saas/env'
+import { createOrganization } from './routes/orgs/create-organization'
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
 
@@ -62,6 +63,8 @@ app.register(createAccount)
 app.register(getProfile)
 app.register(requestPasswordRecover)
 app.register(resetPassword)
+
+app.register(createOrganization)
 
 app.listen({ port: env.SERVER_PORT }).then(() => {
   console.log(`listening on port ${env.SERVER_PORT}`)
---
+++ apps/api/src/utils/create-slug.ts
@@ -0,0 +1,8 @@
+export function createSlug(text: string): string {
+  return text
+    .normalize('NFD')
+    .replace(/[\u0300-\u036f]/g, '')
+    .replace(/[^\w\s]/g, '')
+    .replace(/\s+/g, '-')
+    .toLowerCase()
+}
