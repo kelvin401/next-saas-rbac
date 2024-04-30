6ef0bdea3ddd50805cd13d81961dbd07060a8293
Kelvin Carvalho <kelvin.nirvana@gmail.com>
2024-04-30 08:26:58 -0300

    feat: route to update organization

---
+++ apps/api/src/utils/get-user-permissions.ts
@@ -0,0 +1,12 @@
+import { type Role, userSchema, defineAbilityFor } from '@saas/auth'
+
+export function getUserPermissions(userId: string, role: Role) {
+  const authUser = userSchema.parse({
+    id: userId,
+    role,
+  })
+
+  const ability = defineAbilityFor(authUser)
+
+  return ability
+}
