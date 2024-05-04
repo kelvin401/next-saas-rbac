1ae6c37f1c80ce8017db7dd1790fb571fe2da07e
Kelvin Carvalho <kelvin.nirvana@gmail.com>
2024-05-04 12:07:25 -0300

    feat: route to list pending invites

---
+++ apps/api/src/http/routes/invites/get-pending-invites.ts
@@ -0,0 +1,88 @@
+import { prisma } from '@/lib/prisma'
+import type { FastifyInstance } from 'fastify'
+import type { ZodTypeProvider } from 'fastify-type-provider-zod'
+import { z } from 'zod'
+import { BadRequestError } from '../_errors/bad-request-error'
+import { auth } from '@/http/middlewares/auth'
+import { roleSchema } from '@saas/auth'
+
+export async function getPendingInvites(app: FastifyInstance) {
+  app
+    .withTypeProvider<ZodTypeProvider>()
+    .register(auth)
+    .get(
+      '/pending-invites',
+      {
+        schema: {
+          tags: ['Invite'],
+          summary: 'Get all user pending invites.',
+          security: [{ bearerAuth: [] }],
+          response: {
+            200: z.object({
+              invites: z.array(
+                z.object({
+                  id: z.string().uuid(),
+                  role: roleSchema,
+                  email: z.string().email(),
+                  createdAt: z.date(),
+                  organization: z.object({
+                    name: z.string(),
+                  }),
+                  author: z
+                    .object({
+                      id: z.string().uuid(),
+                      name: z.string().nullable(),
+                      avatarUrl: z.string().url().nullable(),
+                    })
+                    .nullable(),
+                })
+              ),
+            }),
+          },
+        },
+      },
+      async (request, reply) => {
+        const userId = await request.getCurrentUserId()
+
+        const user = await prisma.user.findUnique({
+          where: {
+            id: userId,
+          },
+        })
+
+        if (!user) {
+          throw new BadRequestError('User not found.')
+        }
+
+        const invites = await prisma.invite.findMany({
+          select: {
+            id: true,
+            email: true,
+            role: true,
+            createdAt: true,
+            author: {
+              select: {
+                id: true,
+                name: true,
+                avatarUrl: true,
+              },
+            },
+            organization: {
+              select: {
+                name: true,
+              },
+            },
+          },
+          where: {
+            email: user.email,
+          },
+        })
+
+        if (!invites) {
+          throw new BadRequestError('Invite not found or expired.')
+        }
+
+        return reply.status(204).send({ invites })
+      }
+    )
+}
