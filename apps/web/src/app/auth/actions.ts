936d086aabca6b6fffbe475870558f29eefd8f12
kelvincarvalho <kelvin.nirvana@gmail.com>
2024-07-31 09:15:18 -0300

    feat: implement login with github method

---
+++ apps/web/src/app/auth/actions.ts
@@ -0,0 +1,16 @@
+'use server'
+
+import { redirect } from 'next/navigation'
+
+export async function SignInWithGithub() {
+  const githubSignInURL = new URL('login/oauth/authorize', 'https://github.com')
+
+  githubSignInURL.searchParams.set('client_id', 'c10ea9c4ac4c1d466371')
+  githubSignInURL.searchParams.set(
+    'redirect_uri',
+    'http://localhost:3000/api/auth/callback',
+  )
+  githubSignInURL.searchParams.set('scope', 'user')
+
+  redirect(githubSignInURL.toString())
+}
--- apps/web/src/app/auth/sign-in/sign-in-form.tsx
+++ apps/web/src/app/auth/sign-in/sign-in-form.tsx
@@ -13,6 +13,7 @@ import { Label } from '@/components/ui/label'
 import { Separator } from '@/components/ui/separator'
 import { useFormState } from '@/hooks/use-form-state'
 
+import { SignInWithGithub } from '../actions'
 import { SignInWithEmailAndPassword } from './actions'
 
 export function SignInForm() {
@@ -24,62 +25,66 @@ export function SignInForm() {
     },
   )
   return (
-    <form onSubmit={handleSubmit} className="space-y-4">
-      {success === false && message && (
-        <Alert variant={'destructive'}>
-          <AlertTriangle className="size-4" />
-          <AlertTitle>Sign in failed!</AlertTitle>
-          <AlertDescription>
-            <p>{message}</p>
-          </AlertDescription>
-        </Alert>
-      )}
-      <div className="space-y-1">
-        <Label htmlFor="email">E-mail</Label>
-        <Input name="email" type="email" id="email" />
-
-        {errors?.email && (
-          <p className="text-xs font-medium text-red-500 dark:text-red-500">
-            {errors.email[0]}
-          </p>
+    <div className="space-y-4">
+      <form onSubmit={handleSubmit} className="space-y-4">
+        {success === false && message && (
+          <Alert variant={'destructive'}>
+            <AlertTriangle className="size-4" />
+            <AlertTitle>Sign in failed!</AlertTitle>
+            <AlertDescription>
+              <p>{message}</p>
+            </AlertDescription>
+          </Alert>
         )}
-      </div>
+        <div className="space-y-1">
+          <Label htmlFor="email">E-mail</Label>
+          <Input name="email" type="email" id="email" />
 
-      <div className="space-y-1">
-        <Label htmlFor="password">Password</Label>
-        <Input name="password" type="password" id="password" />
+          {errors?.email && (
+            <p className="text-xs font-medium text-red-500 dark:text-red-500">
+              {errors.email[0]}
+            </p>
+          )}
+        </div>
 
-        {errors?.password && (
-          <p className="text-xs font-medium text-red-500 dark:text-red-500">
-            {errors.password[0]}
-          </p>
-        )}
-        <Link
-          href="/auth/forgot-password"
-          className="text-xs font-medium text-foreground hover:underline"
-        >
-          Forgot your password?
-        </Link>
-      </div>
+        <div className="space-y-1">
+          <Label htmlFor="password">Password</Label>
+          <Input name="password" type="password" id="password" />
 
-      <Button type="submit" className="w-full" disabled={isPending}>
-        {isPending ? (
-          <Loader2 className="size-4 animate-spin" />
-        ) : (
-          'Sign in with e-mail'
-        )}
-      </Button>
+          {errors?.password && (
+            <p className="text-xs font-medium text-red-500 dark:text-red-500">
+              {errors.password[0]}
+            </p>
+          )}
+          <Link
+            href="/auth/forgot-password"
+            className="text-xs font-medium text-foreground hover:underline"
+          >
+            Forgot your password?
+          </Link>
+        </div>
 
-      <Button variant="link" className="w-full" size="sm" asChild>
-        <Link href="/auth/sign-up">Create new account</Link>
-      </Button>
+        <Button type="submit" className="w-full" disabled={isPending}>
+          {isPending ? (
+            <Loader2 className="size-4 animate-spin" />
+          ) : (
+            'Sign in with e-mail'
+          )}
+        </Button>
+
+        <Button variant="link" className="w-full" size="sm" asChild>
+          <Link href="/auth/sign-up">Create new account</Link>
+        </Button>
+      </form>
 
       <Separator />
 
-      <Button type="submit" className="w-full" variant="outline">
-        <Image src={githubIcon} alt="" className="mr-2 size-4 dark:invert" />
-        Sign in with GitHub
-      </Button>
-    </form>
+      <form action={SignInWithGithub}>
+        <Button type="submit" className="w-full" variant="outline">
+          <Image src={githubIcon} alt="" className="mr-2 size-4 dark:invert" />
+          Sign in with GitHub
+        </Button>
+      </form>
+    </div>
   )
 }
