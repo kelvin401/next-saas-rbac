7877a215b92be51e6b4e26a9678aaeb2b06e5b89
kelvincarvalho <kelvin.nirvana@gmail.com>
2024-08-17 21:36:29 -0300

    fix: remove additional header

--- apps/web/src/app/(app)/org/[slug]/page.tsx
+++ apps/web/src/app/(app)/org/[slug]/page.tsx
@@ -1,9 +1,7 @@
-import { Header } from '@/components/header'
-
 export default async function Projects() {
   return (
     <div className="py-4">
-      <Header />
+      <div></div>
       <main></main>
     </div>
   )
