ca1b22b6f67885226b490feedd7bdddc40d78ea1
kelvincarvalho <kelvin.nirvana@gmail.com>
2024-08-17 21:54:41 -0300

    fix: remove additional header

--- apps/web/src/app/(app)/page.tsx
+++ apps/web/src/app/(app)/page.tsx
@@ -1,3 +1,14 @@
+import { Header } from '@/components/header'
+
 export default async function Home() {
-  return <h1>Projects</h1>
+  return (
+    <div className="space-y-4 py-4">
+      <Header />
+      <main className="mx-auto w-full max-w-[1200px] space-y-4">
+        <p className="text-sma text-muted-foreground">
+          Select an organization.
+        </p>
+      </main>
+    </div>
+  )
 }
