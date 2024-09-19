6f7701f84f5383529c5ce8655414c3fe2ef12f02
kelvincarvalho <kelvin.nirvana@gmail.com>
2024-09-18 21:28:31 -0300

    feat: allow to invite members to organization

--- apps/web/src/hooks/use-form-state.ts
+++ apps/web/src/hooks/use-form-state.ts
@@ -1,4 +1,5 @@
 import { FormEvent, useState, useTransition } from 'react'
+import { requestFormReset } from 'react-dom'
 
 interface FormState {
   success: boolean
@@ -36,6 +37,8 @@ export function useFormState(
 
       setFormstate(state)
     })
+
+    requestFormReset(form)
   }
 
   return [FormState, handleSubmit, isPending] as const
