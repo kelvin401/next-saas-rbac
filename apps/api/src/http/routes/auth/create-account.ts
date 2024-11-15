ba0769a4a0479b08903633ce01a12e845961fca3
kelvincarvalho <kelvin.nirvana@gmail.com>
2024-11-15 18:34:36 -0300

    fix: allowed auto join in organization when domain is the same from organization

--- apps/api/src/http/routes/auth/create-account.ts
+++ apps/api/src/http/routes/auth/create-account.ts
@@ -1,17 +1,18 @@
-import { FastifyInstance } from 'fastify'
-import { ZodTypeProvider } from 'fastify-type-provider-zod'
-import { z } from 'zod'
-import { prisma } from '@/lib/prisma'
 import { hash } from 'bcryptjs'
-import { BadRequestError } from '../_errors/bad-request-error'
+import type { FastifyInstance } from 'fastify'
+import type { ZodTypeProvider } from 'fastify-type-provider-zod'
+import { z } from 'zod'
+
+import { BadRequestError } from '@/http/routes/_errors/bad-request-error'
+import { prisma } from '@/lib/prisma'
 
 export async function createAccount(app: FastifyInstance) {
   app.withTypeProvider<ZodTypeProvider>().post(
     '/users',
     {
       schema: {
-        tags: ['User'],
-        summary: 'Create a new account.',
+        tags: ['Auth'],
+        summary: 'Create a new account',
         body: z.object({
           name: z.string(),
           email: z.string().email(),
@@ -22,15 +23,18 @@ export async function createAccount(app: FastifyInstance) {
     async (request, reply) => {
       const { name, email, password } = request.body
 
-      const userWIthSameEmail = await prisma.user.findUnique({
-        where: { email },
+      const userWithSameEmail = await prisma.user.findUnique({
+        where: {
+          email,
+        },
       })
 
-      if (userWIthSameEmail) {
+      if (userWithSameEmail) {
         throw new BadRequestError('User with same e-mail already exists.')
       }
 
       const [, domain] = email.split('@')
+
       const autoJoinOrganization = await prisma.organization.findFirst({
         where: {
           domain,
@@ -56,6 +60,6 @@ export async function createAccount(app: FastifyInstance) {
       })
 
       return reply.status(201).send()
-    }
+    },
   )
 }
