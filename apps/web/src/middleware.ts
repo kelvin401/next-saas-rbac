dccc98a39c2a963be04ea3a9b7116a26d5ab49a5
kelvincarvalho <kelvin.nirvana@gmail.com>
2024-11-17 11:19:27 -0300

    fix: eslint issues

--- apps/web/src/middleware.ts
+++ apps/web/src/middleware.ts
@@ -19,7 +19,7 @@ export function middleware(request: NextRequest) {
 export const config = {
   matcher: [
     /*
-     * Match all request paths except for the ones starting with:
+     * Match all request paths except for the ones starting with :
      * - api (API routes)
      * - _next/static (static files)
      * - _next/image (image optimization files)
